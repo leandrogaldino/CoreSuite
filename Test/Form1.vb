5c93ee1e14a0f65574e629d69550197b95554372
Leandro Correia Galdino <leandrocgaldino@gmail.com>
2026-03-29 13:26:54 -0300

    Update Form1.vb

--- Test/Form1.vb
+++ Test/Form1.vb
@@ -2,57 +2,54 @@
 
 Public Class Form1
     Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim Service As New FirebaseService("", "", "")
-        Await Service.Auth.LoginAsync("reicol@manager.com", "123456")
+        Dim Remote As New FirebaseService("", "", "")
+        Await Remote.Auth.LoginAsync("josias@manager.com", "123456")
+        Dim Docs = Await Remote.Firestore.GetAllDocumentsAsync("evaluations")
 
-        Dim Docs = Await Service.Firestore.GetAllDocumentsAsync("evaluations")
+        Dim Local As New MySqlService("localhost", "manager", "root", "123456")
 
-
-        Service = New FirebaseService("", "", "")
-        Await Service.Auth.LoginAsync("leandro@manager.com", "123456")
-
+        Dim EvaluationID As Long
         For Each Doc In Docs
+
+            Dim LocalResult = Await Local.Request.ExecuteSelectAsync("evaluation", New MySqlSelectOptions() With {
+                .Columns = {"id"}.ToList,
+                .Where = $"cloudid = '{Doc("id")}'"
+            })
+
+            If LocalResult.HasData Then
+                EvaluationID = LocalResult.Data(0)("id")
+            Else
+                EvaluationID = 0
+            End If
+
+
+            Dim info As New Dictionary(Of String, Object) From {
+                {"hasreplacedproducts", False},
+                {"importedby", If(EvaluationID > 0, "LUIZ", Nothing)},
+                {"importeddate", If(EvaluationID > 0, "29/03/2026 00:00:00", Nothing)},
+                {"importedid", If(EvaluationID > 0, EvaluationID, Nothing)},
+                {"importingby", Nothing},
+                {"importingdate", Nothing},
+                {"requestprocessed", True},
+                {"visitscheduleid", Nothing}
+            }
+
+
+            Doc.Add("info", info)
+
+
 
             'If Doc("id") = "4a0393b63-3ee7-4219-ac1b-cd74b35eb55f_23032026_125339214" Then Continue For
 
             'ProcessRequests(Doc)
             'ProcessEvaluations(Doc)
 
-            Await Service.Firestore.SaveDocumentAsync("evaluations", Doc("id"), Doc)
+            Await Remote.Firestore.SaveDocumentAsync("evaluations", Doc("id"), Doc)
 
         Next Doc
 
 
     End Sub
 
-    Private Function ProcessEvaluations(Doc As Dictionary(Of String, Object)) As Dictionary(Of String, Object)
-        Dim info = Doc("info")
 
-        info("importedby") = "LUIZ"
-        info("importeddate") = "10/03/2026 00:00:00"
-
-        Return Doc
-    End Function
-
-
-
-    Private Function ProcessRequests(Doc As Dictionary(Of String, Object)) As Dictionary(Of String, Object)
-
-        Dim replacedProducts = TryCast(Doc("replacedproducts"), IList)
-
-        Dim info = Doc("info")
-
-
-
-        Dim hasProducts As Boolean = False
-        If replacedProducts IsNot Nothing Then
-            hasProducts = replacedProducts.Count > 0
-        End If
-
-        Doc("greasing") = 0
-        info("hasreplacedproducts") = hasProducts
-        info("requestprocessed") = True
-
-        Return Doc
-    End Function
 End Class
